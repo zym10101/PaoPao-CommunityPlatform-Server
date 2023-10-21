4481f09fb5a618df68e83a8bec4fa5c2c197a34f
whm <211250107@smail.nju.edu.cn>
2023-10-21 10:35:02 +0800

    引入satoken和mybatis-plus

--- user-center/src/main/java/com/mise/usercenter/UserCenterApplication.java
+++ user-center/src/main/java/com/mise/usercenter/UserCenterApplication.java
@@ -1,13 +1,17 @@
 package com.mise.usercenter;
 
+import cn.dev33.satoken.SaManager;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.mise.usercenter.mapper")
 public class UserCenterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserCenterApplication.class, args);
+        System.out.println("启动成功，Sa-Token 配置如下：" + SaManager.getConfig());
     }
 
 }
